09c96d0586c0a5864440e2f80be49e26d1e13d26
Ben Johnson <deafgreatdane@gmail.com>
2011-09-24 10:13:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearnSqueel::Application.config.secret_token = '911a57edf2e4135d42b72792a884e883e116ed304e93a606dd2b817b68558156ab22b554a983bd96f965923437e6776bea65bc3eccd57e45cafeba3e33006960'
