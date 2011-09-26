7ffa8dbf7e96f084d6311a8dd2004ece77b77fc9
Ben Johnson <deafgreatdane@gmail.com>
2011-09-26 05:39:08 -0500

    finish up advanced examples

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,8 @@ bob = Person.create({ :first_name => "Bob", :last_name => "Bogart", :birthday =>
 carl = Person.create( :first_name => "Carl", :last_name => "Cupid", :birthday =>'3/1/1972', :state => "MI")
 diane = Person.create( :first_name => "Diane", :last_name => "Debuque", :birthday =>'4/1/1973', :state => "IA")
 
-spot = Pet.create( :name =>"Spot", :kind => "dog", :person =>carl)
+spot = Pet.create( :name =>"Spot", :kind => "dog", :person =>carl , :store => humane_society)
+marble = Pet.create(:name =>"Marble",:kind => "cat",:person => carl, :store => neighbor)
 fluffy = Pet.create( :name =>"Fluffy", :kind => "cat", :person => diane)
 zoom = Pet.create(:name =>"Zoom",:kind =>"dog", :person =>alice , :store =>breeder2 )
 scooby = Pet.create(:name =>"Scooby",:kind => "dog", :person =>alice , :store =>breeder1 )
@@ -23,7 +24,9 @@ scooby = Pet.create(:name =>"Scooby",:kind => "dog", :person =>alice , :store =>
 Toy.create([
            { :pet_id => spot.id , :name => "bone" , :price => 1.29 , :store =>breeder2},
            { :pet_id => spot.id , :name => "frisbee" , :price => 2.39 , :store =>petsmart},
+           { :pet_id => spot.id , :name => "squeeky" , :price => 5.59 , :store =>amazon},
            { :pet => zoom , :name => "frisbee" , :price => 2.39 , :store => petsmart},
            { :pet => zoom , :name => "frisbee" , :price => 2.59 , :store => amazon},
+           { :pet => scooby , :name => "bone" , :price => 3.59 , :store => breeder1},
             { :pet_id => fluffy.id , :name => "yarn" , :price => 0.59, :store =>petsmart }
            ])
